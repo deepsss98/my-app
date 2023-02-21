0bcdb2e016c5d66bbcc096991d7a912c6246ce3d
deepsss98 <deepsss98@gmail.com>
2023-02-21 14:11:40 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Displayname
+        name="Deepika"
+        url="https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+      />
+      <Displayname name="Kumar" url="https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+     />
+      <Displayname
+        name="Amutha"
+        url="https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+      />
     </div>
   );
 }
 
-export default App;
+function Displayname(props) {
+  return (
+    <div className="App1" style={{ color: "brown", textAlign: "center" }}>
+      <h1>Hello {props.name}</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <br />
+      <img className="profile-pic" src={props.url} alt="" />
+    </div>
+  );
+}
